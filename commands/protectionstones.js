5bbb76ea682d0536b570215be11ed026aa66e2c7
kCyfer <68758641+kCyfer@users.noreply.github.com>
2021-04-06 20:40:42 +0200

    Fix the description command

--- commands/protectionstones.js
+++ commands/protectionstones.js
@@ -5,7 +5,7 @@ module.exports = {
     run: (bot, args, message) => {
         const embed = bot.embed
             .setTitle("ProtectionStones")
-            .setDescription('"ProtectionStones to plugin, który pozwala w łatwy sposób tworzyć cuboidy poprzez stawianie >BLOKÓW MOCY<.\nPamiętaj o poprawnej jego konfiguracji zanim zgłosisz udasz się tutaj po pomoc.\n\n[JAK OBSŁUGIWAĆ PROTECTIONSTONES](https://github.com/espidev/ProtectionStones/wiki)')
+            .setDescription('ProtectionStones to plugin, który pozwala w łatwy sposób tworzyć cuboidy poprzez stawianie >BLOKÓW MOCY<.\nPamiętaj o poprawnej jego konfiguracji zanim zgłosisz udasz się tutaj po pomoc.\n\n[JAK OBSŁUGIWAĆ PROTECTIONSTONES](https://github.com/espidev/ProtectionStones/wiki)')
 
 
         const text = args.join(" ");
