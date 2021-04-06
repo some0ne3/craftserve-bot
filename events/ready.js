1355d07506a812b04d4f175ce5ec8ab91e28af70
kCyfer <68758641+kCyfer@users.noreply.github.com>
2021-04-06 17:05:47 +0200

    Adding a changing bot status

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,18 @@
-module.exports = bot => {
+module.exports = async bot => {
     const guild = bot.guilds.cache.get("387222965131149313");
     const memberCount = guild?.memberCount - guild?.members.cache.filter(member => member.user.bot).size;
     console.log("Bot został pomyślnie włączony.");
 
-    bot.user.setPresence({
-        status: 'online',
-        activity: {
-            name: `${memberCount || "TEST"} użytkowników`,
-            type: "LISTENING"
-        }
-    });
+    const activities = [
+        { name: '!help', type: 'LISTENING' },
+        { name: `!pomoc`, type: 'LISTENING' }
+    ];
+    await bot.user.setPresence({status: 'online', activity: activities[0]});
+    let activity = 1;
+    setInterval(() => {
+        activities[2] = { name: `${memberCount} użytkowników`, type: 'WATCHING' };
+        if (activity > 2) activity = 0;
+        bot.user.setActivity(activities[activity]);
+        activity++;
+    }, 10000);
 };
