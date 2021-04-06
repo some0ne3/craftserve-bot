f72bdf95a20927c08c3f9bcff5ac943708f51b3f
kCyfer <68758641+kCyfer@users.noreply.github.com>
2021-04-06 15:43:29 +0200

    Change of bot status to number of users

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,12 @@
 module.exports = bot => {
+    const guild = bot.guilds.cache.get("742114424848122026");
+    const memberCount = guild.members.cache.filter(member => !member.user.bot).size;
     console.log("Bot został pomyślnie włączony.");
 
     bot.user.setPresence({
         status: 'online',
         activity: {
-            name: "!help",
+            name: `${memberCount} użytkowników`,
             type: "LISTENING"
         }
     });
