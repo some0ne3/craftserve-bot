5b18f5be7288d5949193a18335ac541129037ce7
MinerPL <minerpl03@gmail.com>
2021-04-06 15:51:58 +0200

    Add example config, change users in ready event

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,12 @@
 module.exports = bot => {
-    const guild = bot.guilds.cache.get("742114424848122026");
-    const memberCount = guild.members.cache.filter(member => !member.user.bot).size;
+    const guild = bot.guilds.cache.get("387222965131149313");
+    const memberCount = guild?.memberCount - guild?.members.cache.filter(member => member.user.bot).size;
     console.log("Bot został pomyślnie włączony.");
 
     bot.user.setPresence({
         status: 'online',
         activity: {
-            name: `${memberCount} użytkowników`,
+            name: `${memberCount || "TEST"} użytkowników`,
             type: "LISTENING"
         }
     });
--- index.js
+++ index.js
@@ -7,23 +7,25 @@ bot.commands = new Discord.Collection()
 require("./commandHandler")(bot)
 require("./events/eventHandler")(bot);
 
+bot.embed = new Discord.MessageEmbed()
+    .setColor(0x224d21)
+    .setTimestamp()
+
 bot.on('message', message => {
-	if (message.author.bot) {
-		return;
+    if (message.author.bot) {
+        return;
     }
-    if(!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
 
     const command = bot.commands.get(cmd);
 
-    if(!command) return;
+    if (!command) return;
 
-    bot.embed = new Discord.MessageEmbed()
-        .setColor(0x224d21)
-        .setFooter(`Komenda !${cmd} | ${message.author.tag}`)
-        .setTimestamp()
+
+    bot.embed.setFooter(`Komenda !${cmd} | ${message.author.tag}`)
 
     command.run(bot, args, message)
 });
