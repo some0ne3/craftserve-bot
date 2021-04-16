75834924f84de40211ad4c9bfd4653e89bba2c86
MinerPL <minerpl03@gmail.com>
2021-04-16 16:06:38 +0200

    Added automatic addition of slashcommands

--- events/eventHandler.js
+++ events/eventHandler.js
@@ -2,5 +2,6 @@ const requestEvent = (event) => require(`./${event}`);
 
 module.exports = bot => {
     bot.on("ready", async () => requestEvent("ready")(bot));
+    bot.on("ready", async () => requestEvent("createSlash")(bot));
     bot.ws.on(`INTERACTION_CREATE`, (interaction) => requestEvent("slashcommands")(bot, interaction))
 };
