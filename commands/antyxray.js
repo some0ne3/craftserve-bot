ae2e091f2fb6dc1f377045d182e23fb303ed3628
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2021-06-07 01:45:26 +0200

    Add new commands: `onlinemode`, `antyxray`

---
+++ commands/antyxray.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "name": "antyxray",
+    "options": [
+        {
+            "name": "Tekst",
+            "description": "Tekst wyświetlany przed odpowiedzą bota",
+            "type": 3,
+            "required": false,
+        },
+    ],
+    "description": "Anti-Xray w silniku Paper",
+    "aliases": ["antixray", "anti-xray", "anty-xray"],
+    run: (bot, args, message) => {
+        const embed = bot.embed
+            .setTitle("Anti-Xray w paper.yml")
+            .setDescription("Silnik Paper, wraz z jego forkami posiadają wbudowany system **anti-xray**. By z niego skorzystać wystarczy ustawić go w pliku `paper.yml`.\n\n Rekomendowane ustawienia anty-xraya (razem z netherem) znajdziesz [**tutaj**](<https://gist.github.com/stonar96/ba18568bd91e5afd590e8038d14e245e#recommended-settings>)")
+
+        const text = args.join(" ");
+
+        return message.channel.send(text, embed)
+    }
+}
---
+++ commands/onlinemode.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "name": "onlinemode",
+    "options": [
+        {
+            "name": "Tekst",
+            "description": "Tekst wyświetlany przed odpowiedzą bota",
+            "type": 3,
+            "required": false,
+        },
+    ],
+    "description": "Zmiana trybu serwera na no-premium",
+    "aliases": ["online-mode"],
+    run: (bot, args, message) => {
+        const embed = bot.embed
+            .setTitle("Problem z wejściem na serwer premium?")
+            .setDescription('Jeśli przy wchodzeniu na serwer wyświetla ci się taki komunikat: `Failed to log in: Invalid session` (pl. `Nie udało się zalogować: Sesja wygasła`), to oznacza, że twój serwer jest premium.\n Aby to zmienić wystarczy:\n\n' +
+                '**1.** Wyłączyć serwer\n' +
+                '**2.** Wejść w plik `server.properties`\n' +
+                '**3.** Zmienić linijkę `online-mode: true` na `online-mode: false`\n' +
+                '**4.** Zapisać plik\n' +
+                '**5.** Włączyć serwer \n')
+
+        const text = args.join(" ");
+
+        return message.channel.send(text, embed)
+    }
+}
