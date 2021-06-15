2ffb4aa665373839b8b67fc4f403a43c673aaf44
MinerPL <minerpl03@gmail.com>
2021-06-15 22:34:25 +0200

    Fix all know bugs with pluginsearch

--- commands/pluginsearch.js
+++ commands/pluginsearch.js
@@ -17,27 +17,32 @@ module.exports = {
         const filtered = args.filter(a => !regexp.test(a)).join("+");
         const mention = message.mentions?.users.first() || "";
         if (args.length <= 0) return;
-        
-        const searchResource = await (await fetch(`https://api.spiget.org/v2/search/resources/${encodeURI(filtered)}?field=name&size=1&sort=-downloads`)).json();
-        const item = searchResource[0];
 
-        if (!item) return message.react ? message.react('❌') : message.channel.send('❌');
+        try {
+            const searchResource = await (await fetch(`https://api.spiget.org/v2/search/resources/${encodeURI(filtered)}?field=name&size=1&sort=-downloads`)).json();
 
-        const resource = await (await fetch(`https://api.spiget.org/v2/resources/${item.id}`)).json();
-        const author = await (await fetch(`https://api.spiget.org/v2/authors/${item.author.id}`)).json();
-        const category = await (await fetch(`https://api.spiget.org/v2/categories/${item.category.id}`)).json();
-        const version = await (await fetch(`https://api.spiget.org/v2/resources/${item.id}/versions/${item.version.id}`)).json();
+            const item = searchResource[0];
 
-        const testedVersions = resource.testedVersions.length > 0 ? `\nPrzetestowane wersje: \`${resource.testedVersions.join(", ")}\`` : "";
-        const embed = bot.embed
-            .setTitle(resource.name)
-            .setAuthor(author.name, author.icon.url ? (spigotURL + author.icon.url) : "https://static.spigotmc.org/styles/spigot/xenforo/avatars/avatar_male_m.png", spigotURL + `members/${author.name}.${author.id}`)
-            .setURL(spigotURL + `resources/${resource.id}`)
-            .setDescription(resource.tag)
-            .addField("Obecna wersja", `Wersja \`${version.name}\` \nData wydania: \`${new Date(version.releaseDate * 1000).toLocaleDateString()}\` \n\n**[>>POBIERZ<<](https://api.spiget.org/v2/resources/${resource.id}/versions/${version.id}/download)**\n`, true)
-            .addField("Informacje", `Ilość pobrań: \`${resource.downloads}\` \nData wydania: \`${new Date(resource.releaseDate * 1000).toLocaleDateString()}\` \nKategoria: \`${category.name}\` \nOpinie: \`${"⭐".repeat(resource.rating.average.toFixed())}(${resource.rating.count})\` ${testedVersions}`, true)
-            .addField("Inne", `[Pozostałe wersje (${resource.versions.length} wersji)](${spigotURL}resources/${resource.id}/history) \n[Historia zmian (${resource.updates.length} elementów)](${spigotURL}resources/${resource.id}/updates)`, false)
-            .setThumbnail(resource.icon.url ? (spigotURL + resource.icon.url) : "https://static.spigotmc.org/styles/spigot/xenresource/resource_icon.png");
-        return message.channel.send(!!mention ? `<@${mention.id}>` : "", embed);
+            if (!item) return message.react ? message.react('❌') : message.channel.send('❌');
+
+            const resource = await (await fetch(`https://api.spiget.org/v2/resources/${item.id}`)).json();
+            const author = await (await fetch(`https://api.spiget.org/v2/authors/${item.author.id}`)).json();
+            const category = await (await fetch(`https://api.spiget.org/v2/categories/${item.category.id}`)).json();
+            const version = await (await fetch(`https://api.spiget.org/v2/resources/${item.id}/versions/${item.version.id}`)).json();
+
+            const testedVersions = resource.testedVersions.length > 0 ? `\nPrzetestowane wersje: \`${resource.testedVersions.join(", ")}\`` : "";
+            const embed = bot.embed
+                .setTitle(resource.name)
+                .setAuthor(author.name, author.icon.url ? (spigotURL + author.icon.url) : "https://static.spigotmc.org/styles/spigot/xenforo/avatars/avatar_male_m.png", spigotURL + `members/${author.name}.${author.id}`)
+                .setURL(spigotURL + `resources/${resource.id}`)
+                .setDescription(resource.tag)
+                .addField("Obecna wersja", `Wersja \`${version.name}\` \nData wydania: \`${new Date(version.releaseDate * 1000).toLocaleDateString()}\` \n\n**[>>POBIERZ<<](https://api.spiget.org/v2/resources/${resource.id}/versions/${version.id}/download)**\n`, true)
+                .addField("Informacje", `Ilość pobrań: \`${resource.downloads}\` \nData wydania: \`${new Date(resource.releaseDate * 1000).toLocaleDateString()}\` \nKategoria: \`${category.name}\` \nOpinie: \`${"⭐".repeat(resource.rating.average.toFixed())}(${resource.rating.count})\` ${testedVersions}`, true)
+                .addField("Inne", `[Pozostałe wersje (${resource.versions.length} wersji)](${spigotURL}resources/${resource.id}/history) \n[Historia zmian (${resource.updates.length} elementów)](${spigotURL}resources/${resource.id}/updates)`, false)
+                .setThumbnail(resource.icon.url ? (spigotURL + resource.icon.url) : "https://static.spigotmc.org/styles/spigot/xenresource/resource_icon.png");
+            return message.channel.send(!!mention ? `<@${mention.id}>` : "", embed);
+        } catch (e) {
+            return message.react ? message.react('❌') : message.channel.send('❌');
+        }
     }
 }
