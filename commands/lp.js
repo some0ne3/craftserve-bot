6e5153d5c4343ae5d5e6d781772c6ddade9745bb
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2021-11-14 00:22:38 +0100

    Update links for luckperms command

--- commands/lp.js
+++ commands/lp.js
@@ -13,7 +13,7 @@ module.exports = {
     run: (bot, args, message) => {
         const embed = bot.embed
             .setTitle("Uprawnienia na serwerze Minecraft")
-            .setDescription('Wiele pluginów, np. EssentialsX, wymaga nadania graczom permisji by Ci mogli korzystać z poszczególnych komend. \n\nNiestety żaden z silników nie posiada wbudowanego menedżera uprawnień, dlatego należy zainstalować odpowiedni plugin - [LuckPerms](https://ci.lucko.me/job/LuckPerms/), którego dokumentacja jest dostępna [tutaj](https://github.com/lucko/LuckPerms/wiki)')
+            .setDescription('Wiele pluginów, np. EssentialsX, wymaga nadania graczom permisji by Ci mogli korzystać z poszczególnych komend. \n\nNiestety żaden z silników nie posiada wbudowanego menedżera uprawnień, dlatego należy zainstalować odpowiedni plugin - [LuckPerms](https://luckperms.net/download), którego dokumentacja jest dostępna [tutaj](https://luckperms.net/wiki)')
 
 
         const text = args.join(" ");
