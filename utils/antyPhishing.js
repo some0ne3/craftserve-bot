491f7f983eab19f71945ded843832472c2b1e216
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2021-11-14 03:04:19 +0100

    Add scanning through SafeBrowsing and VirusTotal

--- utils/antyPhishing.js
+++ utils/antyPhishing.js
@@ -1,10 +1,86 @@
 import fetch from 'node-fetch';
+import FormData from 'form-data';
+import followRedirect from 'follow-redirect-url';
 
+const getFinalUrl = async (url) => (await followRedirect.startFollowing(url)).pop().url;
+
+const checkVirusTotal = async (string) => {
+	console.log(`[VirusTotal] scanning...`);
+
+	const form = new FormData();
+	form.append('url', string);
+
+	const virusTotalAnalyseIdRes = await fetch('https://www.virustotal.com/api/v3/urls', {
+		headers: { 'x-apikey': process.env.VIRUSTOTAL_KEY },
+		method: 'POST',
+		body: form,
+	}).catch(r => console.log(r));
+	const analyseId = (await (await virusTotalAnalyseIdRes).json()).data?.id;
+	const virusTotalAnalyseRes = await fetch(`https://www.virustotal.com/api/v3/analyses/${analyseId}`, { headers: { 'x-apikey': process.env.VIRUSTOTAL_KEY } }).catch(r => console.log(r));
+	const analyse = await (await virusTotalAnalyseRes).json();
+	const malicious = analyse.data?.attributes?.stats?.malicious;
+	console.log('[VirusTotal]', malicious + 'x malicious');
+	return malicious;
+};
+
+const checkGSB = async (string) => {
+	let finalUrl = await getFinalUrl(string);
+	console.log(`[GSB] scanning(${finalUrl})...`);
+
+	const gsbRes = await fetch('https://transparencyreport.google.com/transparencyreport/api/v3/safebrowsing/status?site=' + finalUrl);
+	let resArr = JSON.parse((await gsbRes.text()).split('\n').pop())[0];
+
+	let title, unsafe, moreInfoArr = [];
+	switch (resArr[1]) {
+	case 5:
+		// unsafe
+		title = 'This site hosts files that are not commonly downloaded';
+		unsafe = true;
+		break;
+	case 3:
+		// unsafe
+		title = 'Some pages on this site are unsafe';
+		unsafe = true;
+		break;
+	case 2:
+		// unsafe
+		title = 'This site is unsafe';
+		unsafe = true;
+		break;
+	case 1:
+		// safe
+		title = 'No unsafe content found';
+		unsafe = false;
+		break;
+	default:
+		// unknown result
+		title = 'No available data';
+		unsafe = false;
+	}
+
+	if (resArr[2] === 1) moreInfoArr.push('Sends visitors to harmful websites');
+	if (resArr[3] === 1) moreInfoArr.push('3rd number has unknown value');
+	if (resArr[4] === 1) moreInfoArr.push('Tries to trick visitors into sharing personal info or downloading software');
+	if (resArr[5] === 1) moreInfoArr.push('Contains unwanted or malicious software');
+	if (resArr[6] === 1) moreInfoArr.push('6th number has unknown value');
+
+	const result = { unsafe, title, moreInfoArr };
+	console.log(`[GSB]`, result);
+
+	return result;
+};
 const checkPhishing = async (message, client) => {
 	const messageArray = message.content.split(/ +/);
+	const regExp = /[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&/=]*)?/gi;
 
 	for (let string of messageArray) {
-		if (client.blockedDomains.includes(string)) return true;
+		if (messageArray.filter(v => client.blockedDomains.some(el => v.includes(el))).length > 0) return true;
+		if (regExp.test(string)) {
+			string = string.match(regExp)[0];
+			console.log(`Found url (${string}) ...`);
+			if ((await checkGSB(string)).unsafe) return true;
+			if (await checkVirusTotal(string) > 0) return true;
+		}
 	}
 	return false;
 };
