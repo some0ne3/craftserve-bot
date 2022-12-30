e2b7c0a789bb2fe14083bdb4d1dc9ecf9d1eecff
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2022-12-30 05:20:15 +0100

    Add google command

---
+++ commands/standard/google.js
@@ -0,0 +1,35 @@
+import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from 'discord.js';
+
+export default {
+	...new SlashCommandBuilder()
+		.setName('google')
+		.setDescription('Zaprowadzi Cię do rozwiązania problemu')
+		.addStringOption(string =>
+			string.setName('content')
+				.setDescription('Słowa które chcesz wyszukać')
+				.setRequired(true),
+		)
+		.toJSON(),
+	async execute(interaction) {
+		const splitted = interaction.options.getString('content').split(' ');
+
+		const regexp = new RegExp('<[@#!&](.*?)>', 'g');
+		const mentions = [], searchedWords = [];
+
+		splitted.forEach(word => {
+			if (regexp.test(word)) {
+				return mentions.push(word);
+			}
+			word && searchedWords.push(word);
+		});
+
+		const searchString = searchedWords.join('+').replaceAll('\n', '+');
+
+		const link = `https://letmegooglethat.com/?q=${searchString}`;
+		const embed = new EmbedBuilder()
+			.setColor(0x224d21)
+			.setTitle('Google')
+			.setDescription(`[${link}](${link})`);
+		return interaction.reply({ embeds: [embed], content: mentions.join('') });
+	},
+};
