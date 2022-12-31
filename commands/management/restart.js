d43adcd563734ecb4667b87cb28ec98ecb4a6124
MinerPL <minerpl03@gmail.com>
2022-12-31 15:10:38 +0100

    Remove console.log, Add no perms message

--- commands/management/restart.js
+++ commands/management/restart.js
@@ -6,12 +6,13 @@ export default {
 	...new SlashCommandBuilder()
 		.setName('restart')
 		.setDescription('Restartuje bota')
-		.toJSON(), async execute(interaction) {
+		.toJSON(),
+	async execute(interaction) {
 		const isManagementServer = (await ServerSettings.findOne({ server_id: interaction.guild.id }))?.is_management_server;
 		const isAdminUser = (await BotAdministrators.findOne({ user_id: interaction.user.id }));
-		console.log(isManagementServer, isAdminUser);
-		if (!isManagementServer || !isAdminUser) return;
-		interaction.reply({ content: 'Trwa restartowanie bota!', ephemeral: true }).then(_ => {
+
+		if (!isManagementServer || !isAdminUser) return interaction.reply({ content: 'Nie masz uprawnień do użycia tej komendy!', ephemeral: true });
+		interaction.reply({ content: 'Trwa restartowanie bota!', ephemeral: true }).then(() => {
 			process.exit(0);
 		});
 	},
