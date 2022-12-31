093fa67a57b8fc34480a74d2a1da09a00d1b3b60
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2022-12-31 04:35:58 +0100

    Add help command

---
+++ commands/standard/help.js
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } from 'discord.js';
+import CustomCommands from '../../models/CustomCommands.js';
+
+export default {
+	...new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Pokazuje menu z komendami')
+		.toJSON(), async execute(interaction) {
+		const commands = await CustomCommands.find({ parent_server_id: interaction.guild.id });
+
+		const generateEmbed = (start) => {
+			const partOfCommands = commands.slice(start, start + 25);
+
+			const embed = new EmbedBuilder()
+				.setTitle('Dostępne komendy serwerowe')
+				.setDescription(`**Strona ${Math.floor(start / 25) + 1}/${Math.ceil(commands.length / 25)}**`)
+				.setColor(0x224d21);
+
+			const fields = partOfCommands.map(cmd => ({
+				name: cmd.command_name, value: cmd.command_description, inline: true,
+			}));
+
+			embed.addFields(...fields);
+
+			return embed;
+		};
+		const getAllCommandsEmbeds = () => {
+			let currentIndex = 0, arr = [];
+			while (currentIndex < commands.length) {
+				arr.push(generateEmbed(currentIndex));
+				currentIndex += 25;
+			}
+			return arr;
+		};
+
+		return interaction.reply({ embeds: getAllCommandsEmbeds(), ephemeral: true });
+	},
+};
