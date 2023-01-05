5577988016f547836ee7280389c8663ab4961575
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2023-01-05 01:57:46 +0100

    Move management check to interactionCreate event

--- commands/management/administrator.js
+++ commands/management/administrator.js
@@ -1,6 +1,4 @@
 import { SlashCommandBuilder } from 'discord.js';
-import ServerSettings from '../../models/ServerSettings.js';
-import BotAdministrators from '../../models/BotAdministrators.js';
 
 export default {
 	...new SlashCommandBuilder()
@@ -8,11 +6,6 @@ export default {
 		.setDescription('Pozwala dodać oraz usunąć administratora bota')
 		.toJSON(),
 	async execute(interaction) {
-		const isManagementServer = (await ServerSettings.findOne({ server_id: interaction.guild.id }))?.is_management_server;
-		const isAdminUser = (await BotAdministrators.findOne({ user_id: interaction.user.id }));
-
-		if (!isManagementServer || !isAdminUser) return interaction.reply({ content: 'Nie masz uprawnień do użycia tej komendy!', ephemeral: true });
-
 		const firstSub = interaction.options.getSubcommandGroup(false) ?? interaction.options.getSubcommand();
 
 		const subCommand = interaction.client.commands.get('administrator').options.find(x => x.name === firstSub);
